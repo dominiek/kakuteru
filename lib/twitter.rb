53e10185634cee233c05dbbc5dcac3058360ce66
Dominiek ter Heide <info@dominiek.com>
2008-09-20 20:34:21 +0900

    Twitter profile image get's fetched automatically now + added .gitignore

---
+++ lib/twitter.rb
@@ -0,0 +1,24 @@
+
+require 'mechanize'
+
+class Twitter
+  
+  def initialize(username)
+    @username = username
+  end
+  
+  def profile_image_url
+    tweets = self.tweets
+    tweets.blank? ? nil : tweets.first['user']['profile_image_url']
+  end
+  
+  def tweets
+    agent = WWW::Mechanize.new
+    response = agent.get("http://twitter.com/statuses/user_timeline/#{@username}.json")
+    if response.code.to_i != 200
+      return nil
+    end
+    ActiveSupport::JSON.decode(response.body)
+  end
+  
+end
