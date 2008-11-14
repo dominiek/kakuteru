c16fe4c6215bdd6ef616e010247cb2ed350742a1
Dominiek ter Heide <info@dominiek.com>
2008-11-15 02:16:27 +0900

    Service profile update routine

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,6 +2,10 @@ class Service < ActiveRecord::Base
   belongs_to :stream
   
   def after_create
+    update_profile!
+  end
+
+  def update_profile!
     if identifier == 'twitter'
       # Fetch profile_image_url
       twitter = Twitter.new(profile_url.match(/twitter\.com\/([^\/]+)/)[1])
