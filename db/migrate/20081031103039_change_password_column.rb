1fa48a2f57993a559f896f59947cdc623ef733e1
Dominiek ter Heide <info@dominiek.com>
2008-11-24 20:26:01 +0900

    Woopsy

--- db/migrate/20081031103039_change_password_column.rb
+++ db/migrate/20081031103039_change_password_column.rb
@@ -1,6 +1,6 @@
 class ChangePasswordColumn < ActiveRecord::Migration
   def self.up
-    #remove_column :streams, :password
+    remove_column :streams, :password
     add_column :streams, :password, :string, :limit => 128
   end
 
