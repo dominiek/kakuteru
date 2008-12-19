0ea67830a33c44603d9c565d04131417f739169b
Dominiek ter Heide <info@dominiek.com>
2008-12-19 17:52:22 +0100

    Changes to invitation flow

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,8 @@ config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
-config.action_mailer.raise_delivery_errors = false
+config.action_mailer.delivery_method = :sendmail
+config.action_mailer.raise_delivery_errors = true
 
 config.active_record.allow_concurrency = true
 Spawn::method :fork, 'development'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,8 @@ config.action_view.cache_template_loading            = true
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
 # Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
+config.action_mailer.delivery_method = :sendmail
+config.action_mailer.raise_delivery_errors = true
 
 
 config.active_record.allow_concurrency = true
