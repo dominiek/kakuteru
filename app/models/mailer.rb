aa048b242896dd86355a9bbce364bcb23a3a4030
Dominiek ter Heide <info@dominiek.com>
2008-12-23 00:13:51 +0100

    Small tweaks to forgot password mechanism

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -3,7 +3,7 @@ class Mailer < ActionMailer::Base
   def forgot_password(stream)
     recipients(stream.email)
     from("no-reply@kakuteru.com")
-    subject("Your #{stream.blog_url} password")
+    subject("Your #{stream.friendly_url} password")
     body(:stream => stream)
   end
 
