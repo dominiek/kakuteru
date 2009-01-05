72812a383c88e4176d0b688ce9dd11bbb7861f7f
Dominiek ter Heide <info@dominiek.com>
2009-01-05 13:43:34 +0100

    Pimped up the slideshow on /

--- app/controllers/intro_controller.rb
+++ app/controllers/intro_controller.rb
@@ -1,10 +1,22 @@
 class IntroController < ApplicationController
   
   def index
+    @intro = [
+      [:your_style, "Lifestreaming, YOUR style"],
+      [:activity, "Activity all over the place"],
+      [:web30_enabled, "Web 3.0 Enabled"],
+      [:integration, "An Integration Behemoth"],
+      [:statistics, "Activity Stats"],
+      [:clusters, "Soon: Cluster Streams"]
+    ]
     @title = 'Kakuteru - Lifestreaming 3.0'
     unless params[:invite_code].blank?
       redirect_to('/signup?invite_code='+params[:invite_code])
     end
+  end
+  
+  def design
+    render(:layout => false)
   end
   
   def signup
